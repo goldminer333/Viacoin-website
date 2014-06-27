c2a53949f473dadda0aa9dfea90958201c7873aa
goldminer333 <astawicki@gmail.com>
2014-06-27 02:34:33 -0700

    UI changes/ layout

---
+++ index.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Viacoin</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/offcanvas.css" rel="stylesheet">
+    
+    <!-- Font Awesome -->
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <div class="navbar navbar-fixed-top navbar-inverse" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            	<li><a href="#viacoin">Viacoin</a></li>
+				<li><a href="#wallets">Wallets</a></li>
+				<li><a href="#specifications">Specifications</a></li>
+				<li><a href="#exchanges">Exchanges</a></li>
+				<li><a href="#community">Community</a></li>
+				<li><a href="#resources">Resources</a></li>
+          </ul>
+        </div><!-- /.nav-collapse -->
+      </div><!-- /.container -->
+    </div><!-- /.navbar -->
+
+    <div class="container">
+
+      <div class="row row-offcanvas row-offcanvas-right">
+
+        <div class="col-xs-12 col-sm-9">
+          <p class="pull-right visible-xs">
+            <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
+          </p>
+         
+         <div class="container" id="content" data-spy="scroll" data-target=".nav">
+		  <div class="col-md-9">
+          <div class="jumbotron">
+            <h1>Hello, world!</h1>
+            <p>This is an example to show the potential of an offcanvas layout pattern in Bootstrap. Try some responsive-range viewport sizes to see it in action.</p>
+          </div>
+			<section id="wallets" class="bg-grey">
+				<div class="row">
+					<div class="col-sm-12">
+						<h2>Wallets</h2>
+						<div class="col-sm-3">
+							<img src="images/git.png" alt="git"/>
+						</div>
+						<div class="col-sm-3">
+							<img src="images/win.png" alt="git"/>
+						</div>
+						<div class="col-sm-3">
+							<img src="images/lin.png" alt="git"/>
+						</div>
+						<div class="col-sm-3">
+							<img src="images/mac.png" alt="git"/>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section id="specifications" class="bg-white">
+				<div class="row">
+					<div class="col-sm-12">
+						<h2>Specifications</h2>
+						<p>
+							Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.
+						</p>
+					</div>
+				</div>
+			</section>
+			<section id="exchanges" class="bg-grey">
+				<div class="row">
+					<div class="col-sm-4">
+						<img class="img-responsive" src="//placehold.it/250x250/918265/FFF">
+					</div>
+					<div class="col-sm-8">
+						<h2>Exchanges</h2>
+						<p>
+							Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.
+						</p>
+					</div>
+				</div>
+			</section>
+			<section id="community" class="bg-white">
+				<div class="row">
+					<div class="col-sm-8">
+						<h2>Community</h2>
+						<p>
+							Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.
+						</p>
+					</div>
+					<div class="col-sm-4"><img class="img-responsive" src="//placehold.it/250x250/918265/FFF">
+					</div>
+				</div>
+			</section>
+			<section id="resources" class="bg-grey">
+				<div class="row">
+					<div class="col-sm-4"><img class="img-responsive" src="//placehold.it/250x250/918265/FFF">
+					</div>
+					<div class="col-sm-8">
+						<h2>Resources</h2>
+						<p>
+							Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.
+						</p>
+					</div>
+				</div>
+			</section>
+        </div><!--/span-->
+
+        <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar" role="navigation">
+          <div class="list-group">
+            <a href="#" class="list-group-item active">Link</a>
+            <a href="#" class="list-group-item">Link</a>
+            <a href="#" class="list-group-item">Link</a>
+            <a href="#" class="list-group-item">Link</a>
+          </div>
+          <div class="well">
+					<h4>1 Viacoin = 0.0005 BTC</h4> 
+				</div>
+				<div id="twitter">
+					
+                    </div>
+        </div><!--/span-->
+      </div><!--/row-->
+
+      <hr>
+
+      <footer>
+        <p>&copy; Company 2014</p>
+      </footer>
+
+    </div><!--/.container-->
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.smooth-scroll.min.js" type="text/javascript"></script>
+    <script src="js/index.js"></script>
+    <!--Sticky Sidebar-->
+    <script src="js/sticky.js">
+    </script>
+    <!--Off Canvas Toggle-->
+    <script>
+    	$(document).ready(function () {
+  		$('[data-toggle="offcanvas"]').click(function () {
+    	$('.row-offcanvas').toggleClass('active')
+  		});
+		});
+    </script>
+    <!--Smooth Scroll-->
+    <script>
+		$('a').smoothScroll();
+		$('body').scrollSpy();
+		$(document).ready(function() {
+		});
+	</script>
+  </body>
+</html>
